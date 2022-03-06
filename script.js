34017fa9255df99c64a464de480644a1c4d48adf
Marygraceee <ma.grasso1999@gmail.com>
2022-03-06 23:56:12 +0100

    added functionalities

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+let errortext = document.getElementById("errormsg")
+let password = document.getElementById("password")
+let confirmpsw = document.getElementById("confirm")
+
 function checkpsw() {
+    
     document.addEventListener("click", e => {
         if(e.target.className === "createacc"){
-            let password = document.getElementsByClassName(".psw")
-  let confirmPassowrd = document.getElementsByClassName(".confirmpsw")
-  if (confirmPassowrd !== password){
-      console.log("ciao!")
+
+  
+  if (confirmpsw.value !== password.value){
+      errortext.textContent = "*passwords don't match"
+      password.style.cssText = "border: 2px solid red"
+      confirmpsw.style.cssText = "border: 2px solid red"
   }
+  if (confirmpsw.value === password.value){
+      errortext.textContent = ""
+      password.style.cssText = "border: 1px solid black"
+      confirmpsw.style.cssText = "border: 1px solid black"
+  }
+  
         }
     })
 }
